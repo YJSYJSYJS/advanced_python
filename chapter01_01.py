b6d21c99a80862d42224214868591e7f896b504b
YounJongSu <dbs106@gmail.com>
2020-06-02 23:08:45 +0900

    절차지향 예시
    절차지향도 CT나 XRay 등에서 쓰이기도 한다.
    무조건 나쁜 방식은 아니지만, 보통은 객체지향이 훨씬 편리하다.
    절차지향을 먼저 해보았고, 다음에 객체지향을 볼 예정

---
+++ chapter01_01.py
@@ -0,0 +1,30 @@
+# Chapter01-1
+# 파이썬 심화
+# 객체 지향 프로그래밍(OOP) -> 코드의 재사용, 코드 중복 방지 등...
+# Today: 
+# 클래스 상세 설명
+# 클래스 변수, 인스턴스 변수
+
+# cf.절차지향
+## 학생 집단을 표현
+### 학생 1
+student_name_1 = 'Kim'
+student_number_1 = 1
+student_grade_1 = 1
+student_detail_1 = [{'gender': 'Male'}, 
+{'score1': 95}, 
+{'score2': 88}
+]
+
+### 학생 2
+student_name_1 = 'Lee'
+student_number_1 = 2
+student_grade_1 = 2
+student_detail_1 = [{'gender': 'Female'}, {'score1': 90}, {'score2': 89}]
+
+### 학생 3
+student_name_1 = 'Youn'
+student_number_1 = 3
+student_grade_1 = 3
+student_detail_1 = [{'gender': 'Male'}, {'score1': 99}, {'score2': 99}]
+###.......여러명 할 것 생각하면 머리가 아프다...
